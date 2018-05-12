a7f482b0fd8731e39dec5a3008f9c7854d46755e
Scott Johnson <sjwvuie@yahoo.com>
2018-05-12 01:29:15 -0400

    updated files

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# unit-4-game
-Dynamically updates HTML pages with the jQuery library
+# Crystals Collector
+### A selection game that dynamically updates HTML pages with the jQuery library
+
+#### 05/011/18 - Scott Johnson
+
+## Project Goal
+The Crystals Collector game demonstrates the use of the jQuery library to dynamically update HTML pages.  Events are included for capturing the user's image clicks and then executes through a sequence of logic steps to determine if the users score has exceeded the score to guess until which time the user either wins (guesses the score exactly) or overruns the score.
+
+## Project Usefulness
+This project incorporates jQuery learnings on how to correctly select and manipulate HTML elements within the DOM.
+
+## Help, Support, and Maintenance
+Users can reach out to me (sjohnson.wvu@gmail.com) for support as I'm the sole contributor to this project.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,67 +1,144 @@
-// Create a variable for the target number to be guessed
-var targetNumber = 50;
+// The random number shown at the start of the game should be between 19 - 120.
+// Create variables for this min / max range
+var targetMax = 120;
+var targetMin = 19;
 
-// FROM THE CRYSTAL EXAMPLE, EDIT THIS LATER...
-// Here we set the "number-to-guess" header to match the "targetNumber".
-// Eventually this will allow us to change the HTML to match the value in the JavaScript.
-$("#number-to-guess").text(targetNumber);
+// Create a variable for the random target number
+var targetNumber = Math.floor(Math.random() * targetMax) + targetMin;
 
-// Create a variable for the counter that tracks the user's total.  We'll increment this each click.
+// Set the number to guess id to display the target number
+$("#numberToGuess").text(targetNumber);
+
+// Create a variable for the counter that tracks the user's total
 var counter = 0;
 
-// var numberOptions = [10, 5, 3, 1];
+// Set the user's score id to display the counter
+$("#userScore").text(counter);
+
+// Each crystal should have a random hidden value between 1 - 12.
+// Create variables for this min / max range
+var numOptionMax = 12;
+var numOptionMin = 1;
 var numberOptions = [];
 var arraySize = 4;
 
-// Create an array of randomly generated unique values to serve as the number options
-// to be assigned to each crystal
+// Create variables for the user's wins and losses
+var wins = 0;
+$("#wins").text(wins);
+var losses = 0;
+$("#losses").text(losses);
 
+// Set the statement
+var statement = "Select a crystal to update your score";
+$("#statement").text(statement);
+
+
+// Create an array of randomly generated unique values between
+// the max and min number to serve as the number options to be
+// assigned to each crystal image
 while (numberOptions.length < arraySize) {
-  var random = Math.floor(Math.random() * 15) + 1;
+  var random = Math.floor(Math.random() * numOptionMax) + numOptionMin;
   if (numberOptions.indexOf(random) == -1) {
     numberOptions.push(random);
   }
 }
 
-// for (var i = 0; i < arraySize; i++) {
-//   var random = Math.floor(Math.random() * 15) + 1;
-//   if (numberOptions[i] != random) {
-//     numberOptions.push(random);
-//   }
-//   else {
-//     numberOptions.push(random - 1);
-//   }
-// }
-
 console.log(numberOptions);
 
-// Give each crystal a different data value
-var imageCrystal1 = $("#crystal1");
-imageCrystal1.attr("dataCrystalValue", numberOptions[0]);
+// Create an image array where each index is equal to the folder
+// path of the images in the image folder
+var imgArray = ["assets/images/crystal1.jpg", "assets/images/crystal2.jpg", "assets/images/crystal3.jpg", "assets/images/crystal4.jpg"]
 
-console.log(imageCrystal1.dataCrystalValue);
+// Set the src equal to the image array index for each of the four images
+// and append these images into the image row.  Also, during the loop, 
+// create the numValue attribute and set it to the numberOptions[i] index.
 
-// Set an on-click event that responds to button clicks of crystal1.
-$("#crystals").on("click", ".crystal-image", function () {
-  counter += 10;
-  alert("Your new score is: " + counter);
+var imgRow = $(".imgRow");
 
-  var crystalValue = ($(this).attr("data-crystalvalue"));
+for (var i = 0; i < arraySize; i++) {
+  var imgCrystal = $("<img>");
+  // give the image a crystalImage class
+  imgCrystal.addClass("crystalImage");
+  // give the image an id
+  imgCrystal.attr("id", i + 1);
+  // link the image to a source in the imgArray
+  imgCrystal.attr("src", imgArray[i]);
+  // give the image an alt value
+  imgCrystal.attr("alt", "Crystal " + (i + 1));
+  // give the image a dataValue
+  imgCrystal.attr("dataValue", numberOptions[i]);
+  // add the image crystal to the page in the image row
+  imgRow.append(imgCrystal);
+}
+
+// Set an on-click event that responds to clicks on the crystals.
+imgRow.on("click", ".crystalImage", function () {
+
+  // The $(this) keyword extracts the dataValue of the clicked crystal, while the .attr("dataValue")
+  // allows us to grab the value out of the "dataValue" attribute.  The string element then needs
+  // to be converted to an integer and added to the counter.
+  var crystalValue = ($(this).attr("dataValue"));
   crystalValue = parseInt(crystalValue);
-  // We then add the crystalValue to the user's "counter" which is a global variable.
-  // Every click, from every crystal adds to the global counter.
   counter += crystalValue;
+  // Update 'your score'
+  $("#userScore").text(counter);
 
-  // Check if the click counter matches the targetNumber.
-  // This click event will be triggered with each click.
-  // If the numbers match user wins, else if the counter exceeds the target number the user loses.
+  // Win-loss logic
   if (counter === targetNumber) {
-    alert("You win!");
+    statement = "YOU WIN!!!";
+    $("#statement").text(statement);
+    $("#statement").css('color','#000000');
+    wins++;
+    $("#wins").text(wins);
+    setTimeout(newGame, 1000 * 3);
   } else if (counter >= targetNumber) {
-    alert("You lose!!");
+    statement = "Sorry, you lose...";
+    $("#statement").text(statement);
+    $("#statement").css('color','#EF5350');
+    $("#statement").css('font-weight','bold');
+    losses++;
+    $("#losses").text(losses);
+    setTimeout(newGame, 1000 * 3);
   }
 });
 
-// Each imageCrystal will be given a data attribute called data-crystalValue.
-// This data attribute will be set equal to the array value.
-// imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+// 2.  Need to create something that generates a new number to guess
+// after each win  or loss.  
+//4.  Need to link this to my portfolio.
+
+function newGame() {
+  // Reset the target number
+  targetNumber = Math.floor(Math.random() * targetMax) + targetMin;
+
+  // Reset the number to guess id to display the new target number
+  $("#numberToGuess").text(targetNumber);
+
+  // Reset the counter that tracks the user's total
+  counter = 0;
+
+  // Reset the user's score id to display the new counter
+  $("#userScore").text(counter);
+
+  // Reset the number options
+  numberOptions = [];
+
+  while (numberOptions.length < arraySize) {
+    random = Math.floor(Math.random() * numOptionMax) + numOptionMin;
+    if (numberOptions.indexOf(random) == -1) {
+      numberOptions.push(random);
+    }
+  }
+
+  console.log(numberOptions);
+
+  // Give the images new values
+  $("#1").attr("dataValue", numberOptions[0]);
+  $("#2").attr("dataValue", numberOptions[1]);
+  $("#3").attr("dataValue", numberOptions[2]);
+  $("#4").attr("dataValue", numberOptions[3]);
+
+  // Reset the statement
+  var statement = "Select a crystal to update your score";
+  $("#statement").text(statement);
+  $("#statement").css('color','#fff');
+}
